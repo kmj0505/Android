34da05b0819538575c6492feec452af16c4a2363
kmj0505 <menge0505@naver.com>
2023-02-02 20:15:59 +0900

    3일차 손가락 버튼 누르기

---
+++ 3일차/FingerButton1.java
@@ -0,0 +1,14 @@
+package com.kcci.sampledrawable;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FingerButton1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fingerbutton);
+    }
+}
