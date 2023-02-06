2a1ce32356ef5ddf644a083c0a86aa3955baa4b6
kmj0505 <menge0505@naver.com>
2023-02-06 20:41:09 +0900

    재업로드

--- 4일차/JavaSocket/ServerKshExample.java
+++
@@ -1,8 +0,0 @@
-package kcci.socket;
-
-public class ServerKshExample {
-	public static void main(String[] args) {
-		ServerKshThread serverKshThread =  new ServerKshThread(); //Thread파일에 생성자가 없으므로 default 생성자 자동 생성
-		serverKshThread.start();
-	}
-}
